17db124bd17625c7025c0e91f33c48472d58b6d6
theo-code1 <codingbusiness99@gmail.com>
2025-04-16 23:56:07 +0100

    create files and push change

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Dashboard from "./Components/Daskboard";
+
+const App = () => { 
+  return (
+    <section className="w-screen h-auto">
+      <Dashboard />
+    </section>
+  );
+}
+export default App;
